5e3b4150e5ae8f024df5d5b8f6a437f328a504b5
fxavio <xavier.rucahobatinya@amalitech.com>
2024-05-31 16:32:30 +0200

    commit

---
+++ src/main/java/org/devopsdemo/devopsdemo/DevopsDemoApplication.java
@@ -0,0 +1,20 @@
+package org.devopsdemo.devopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsDemoApplication {
+
+    @GetMapping
+    public String message(){
+        return "welcome to devops demo application";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsDemoApplication.class, args);
+    }
+
+}
